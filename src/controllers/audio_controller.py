7368580666fa925cc7cde97d925739a1b1c052b4
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2024-05-22 09:26:44 -0300

    feat implement save audio received

--- src/controllers/audio_controller.py
+++ src/controllers/audio_controller.py
@@ -1,24 +1,36 @@
 from flask import Blueprint, request, jsonify
-
+from pydub import AudioSegment
 from src.controllers.validators.classify_audio_validator import classify_audio_validator
-
 from src.types.errors.bad_request import BadRequest
 from src.types.http.response import HttpResponse
-
 from src.business.classify_audio_use_case import ClassifyAudioUseCase
+import os
+
+
+uploads_folder = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'uploads')
+if not os.path.exists(uploads_folder):
+    os.makedirs(uploads_folder)
+
 
 audio_routes_blueprint = Blueprint('audio_routes', __name__)
 
 @audio_routes_blueprint.route('/api/audio/classify', methods = ['POST'])
 def classify_audio():
     try:
-        classify_audio_validator(request)
+        # classify_audio_validator(request)
 
         audio = request.files['audio']
+        print("-------")
+        filename = os.path.join(uploads_folder, audio.filename)
+        audio.save(filename)
 
-        response = ClassifyAudioUseCase().classify(audio)
+        # Manipular o arquivo (exemplo: converter para wav)
+        audio = AudioSegment.from_file(filename)
 
-        return jsonify(response)
+
+        #response = ClassifyAudioUseCase().classify(audio)
+
+        return jsonify('1')
     except Exception as error:
         # if isinstance(error, BadRequest):
         #     return HttpResponse(
--- src/infrastructure/spectrogram_handler.py
+++ src/infrastructure/spectrogram_handler.py
@@ -1,2 +1,20 @@
-def create_spectrogram(audio):
-    return 'audio.png'
+import librosa
+import librosa.display
+import numpy as np
+import matplotlib.pyplot as plt
+
+def create_spectrogram(audio_file: str):
+    y, sr = librosa.load(audio_file)
+
+    spec = np.abs(librosa.cqt(y, sr = sr))
+    spec = librosa.amplitude_to_db(spec, ref = np.max)
+
+    librosa.display.specshow(spec, y_axis = 'CQT', fmax = 8000, x_axis = 'Time')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Spectrogram')
+    plt.show()
+
+    # plt.imsave(output_image_file, C, cmap='gray')
+    # image = img.open(output_image_file)
+    # new_image = image.resize((256, 256))
+    # new_image.save(output_image_file)
