dc71734db9be52282e01767612f1b81ef04b6eef
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2024-06-27 18:16:54 -0300

    feat: add waitress WSGI server

--- run.py
+++ run.py
@@ -1,4 +1,13 @@
 from src.server import app
+from waitress import serve
+
+mode = 'dev'
+
+host = '0.0.0.0'
+port = 8080
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 8080, debug = True)
+    if mode == 'dev':
+        app.run(host = host, port = port, debug = True)
+    else:
+        serve(app, host = host, port = port, threads = 5)
