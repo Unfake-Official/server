e304f5a3c1e3d7e3eaa3673d77cd9dfe87e4505b
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2024-07-13 17:54:51 -0300

    feat: setup cors

--- src/server.py
+++ src/server.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 from src.controllers.audio_controller import audio_routes_blueprint
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(audio_routes_blueprint)
